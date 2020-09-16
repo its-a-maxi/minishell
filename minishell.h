f03d39bf62e6407d549dc87f961b9e6c6971005d
Alejandro Leon castell <aleon-ca@c1r5s4.42madrid.com>
2020-09-16 09:25:07 +0200

    Added header file.

---
+++ minishell.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aleon-ca <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/16 09:09:03 by aleon-ca          #+#    #+#             */
+/*   Updated: 2020/09/16 09:24:28 by aleon-ca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fctnl.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <dirent.h>
+# include <string.h>
+# include <errno.h>
+
+#endif
