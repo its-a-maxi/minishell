4ad16f95775ccbf10f3e28456d515804ed41fbd3
Alejandro Leon castell <aleon-ca@c1r5s4.42madrid.com>
2020-09-21 11:09:22 +0200

    Added the structs that the parser will use

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: aleon-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/16 09:09:03 by aleon-ca          #+#    #+#             */
-/*   Updated: 2020/09/17 13:22:10 by alejandro        ###   ########.fr       */
+/*   Updated: 2020/09/21 11:08:09 by aleon-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,38 @@
 # include <string.h>
 # include <errno.h>
 # include "../libft/inc/libft.h"
+
+/*
+** The t_simple_commands struct contains the name and arguments of
+** an input command:
+**
+** command_name arguments[1] ... arguments[n]
+*/
+
+typedef struct	s_simple_command
+{
+	char		*command_name;
+	char		**arguments;
+}				t_simple_command;
+
+/*
+** The t_command_table struct contains the information of a set of
+** input commands, i.e, some input line of the form:
+** 
+** command_name arguments | command_name2 ... | ... > output > input >> append;
+**
+** which we will use to stablish the correct pipe and redirection relations
+** between commands.
+*/
+
+typedef struct			s_command_table
+{
+	t_simple_command	*commands;
+	char				*input_fd;
+	char				*output_fd;
+	char				*append_fd;
+}						t_command_table;
+
 
 char		*ft_add_char(char *str, char c);
 void		read_input(char *input);
