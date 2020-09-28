2305bd0af278c1837414c6cd27d48a87a0273db7
Maxi <mmonroy-@Maxis-iMac.local>
2020-09-28 10:52:53 +0200

    cmd_unset added, test is necesary

--- src/cmd_unset.c
+++ src/cmd_unset.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cmd_export.c                                       :+:      :+:    :+:   */
+/*   cmd_unset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmonroy- <mmonroy-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/24 11:24:48 by mmonroy-          #+#    #+#             */
-/*   Updated: 2020/09/28 10:24:30 by mmonroy-         ###   ########.fr       */
+/*   Created: 2020/09/28 10:13:01 by mmonroy-          #+#    #+#             */
+/*   Updated: 2020/09/28 10:51:53 by mmonroy-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,59 +20,51 @@ static void     free_double(char **str)
 	while(str[i])
 		free(str[i++]);
 	free(str);
+    return;
 }
 
-static void     new_g_env(char *str)
+static void     del_g_env(char *arg)
 {
     char    **temp;
     char    **buff;
     int     i;
+    int     len;
 
     i = -1;
-    temp = (char**)ft_calloc(envp_len(g_env) + 2, sizeof(char*));
-    while (g_env[++i] && (g_env[i + 1][0] != '_' && g_env[i + 1][1] != '='))
-	    if(!(temp[i] = ft_strdup(g_env[i])))
-		    exit_minishell();
-    if(!(temp[++i] = ft_strdup(str)))
-	    exit_minishell();
-    i++;
-    if(!(temp[i] = ft_strdup(g_env[i - 1])))
-	    exit_minishell();
+    len = ft_strlen(arg);
+    temp = (char**)ft_calloc(envp_len(g_env), sizeof(char*));
+    while (g_env[++i])
+        if (ft_strncmp(arg, g_env[i], len) != 0)
+            if(!(temp[i] = ft_strdup(g_env[i])))
+                exit_minishell();
     buff = g_env;
     g_env = temp;
     free_double(buff);
+    return;
 }
 
 static int      ft_check(char *str)
 {
     int i;
+    int len;
 
     i = -1;
-    while (str[++i])
-        if (str[i] != 0 && str[i + 1] == '=')
+    len = ft_strlen(str);
+    while (g_env[++i])
+        if (ft_strncmp(str, g_env[i], len) == 0)
             return (1);
     return (0);
 }
 
-void            cmd_export(char **arg)
+void            cmd_unset(char **arg)
 {
     int i;
 
     i = 0;
     if (!arg[1])
-    {
-        while (g_env[i] || (g_env[i][0] != '_' && g_env[i][1] != '='))
-        {
-            write(1, g_env[i], ft_strlen(g_env[i]));
-            write(1, "\n", 1);
-            i++;
-        }
-        return;
-    }
+        write(1, "unset: not enough arguments", 27);
     while (arg[++i])
-    {
         if (ft_check(arg[i]))
-            new_g_env(arg[i]);
-    }
+            del_g_env(arg[i]);
     return;
 }
