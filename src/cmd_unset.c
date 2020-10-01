95bf837c5bcb905336884db726a65b675a0e6860
Maxi <mmonroy-@Maxis-iMac.local>
2020-10-01 13:10:27 +0200

    cmd_unset corrected

--- src/cmd_unset.c
+++ src/cmd_unset.c
@@ -6,11 +6,23 @@
 /*   By: mmonroy- <mmonroy-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 10:13:01 by mmonroy-          #+#    #+#             */
-/*   Updated: 2020/09/28 10:51:53 by mmonroy-         ###   ########.fr       */
+/*   Updated: 2020/10/01 13:09:40 by mmonroy-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	ft_find_char(char *str, char c)
+{
+	int i;
+
+	i = 0;
+	while (str[i] && str[i] != c)
+		i++;
+	if (str[i] == c)
+		return (i);
+	return (-1);
+}
 
 static void     free_double(char **str)
 {
@@ -28,15 +40,15 @@ static void     del_g_env(char *arg)
     char    **temp;
     char    **buff;
     int     i;
-    int     len;
+    int     j;
 
     i = -1;
-    len = ft_strlen(arg);
+    j = -1;
     temp = (char**)ft_calloc(envp_len(g_env), sizeof(char*));
     while (g_env[++i])
-        if (ft_strncmp(arg, g_env[i], len) != 0)
-            if(!(temp[i] = ft_strdup(g_env[i])))
-                exit_minishell();
+        if (ft_strncmp(arg, g_env[i], ft_find_char(g_env[i], '=')) != 0)
+            if(!(temp[++j] = ft_strdup(g_env[i])))
+                exit_minishell ();
     buff = g_env;
     g_env = temp;
     free_double(buff);
@@ -50,6 +62,14 @@ static int      ft_check(char *str)
 
     i = -1;
     len = ft_strlen(str);
+    if (ft_find_char(str, '=') != -1)
+    {  
+        write(1, "unset: ", 7);
+        write(1, str, len);
+        write(1, ": invalid parameter name\n", 25);
+        return (0);
+    }
+    i = -1;
     while (g_env[++i])
         if (ft_strncmp(str, g_env[i], len) == 0)
             return (1);
@@ -62,7 +82,7 @@ void            cmd_unset(char **arg)
 
     i = 0;
     if (!arg[1])
-        write(1, "unset: not enough arguments", 27);
+        write(1, "unset: not enough arguments\n", 28);
     while (arg[++i])
         if (ft_check(arg[i]))
             del_g_env(arg[i]);
